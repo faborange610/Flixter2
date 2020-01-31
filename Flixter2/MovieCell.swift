30838e5e3a81945382af24592b3e0c689d7ccb90
Songeun Kim <faborange610@gmail.com>
2020-01-30 19:34:16 -0800

    Flixter 2 Part 2 - Assignment 2

---
+++ Flixter2/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flixter2
+//
+//  Created by 김송은 on 1/29/20.
+//  Copyright © 2020 faborange610@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flixter2/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  Flixter2
+//
+//  Created by 김송은 on 1/30/20.
+//  Copyright © 2020 faborange610@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+}
